cb26ec499c1ede2d2b766ed7555eb86c54bf3223
chpag <115650339+chpag@users.noreply.github.com>
2024-09-03 23:58:50 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -44,6 +44,10 @@ variable "width" {
   description = "Image width in pixels."
 }
 
+variable "image" {
+  description = "Image to generate"
+}
+
 variable "placeholder" {
   default     = "loremflickr.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
