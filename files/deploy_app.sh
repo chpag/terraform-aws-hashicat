51f36d0a5ad610395c974f996592f0a0c1044201
chpag <115650339+chpag@users.noreply.github.com>
2024-09-03 23:56:41 +0200

    Dynamic Image deploy_app

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,8 +9,8 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>CPAG Dynamic Creds: Meow World!</h2></center>
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}/${IMG}"></img></center>
+  <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
   <!-- END -->
 
